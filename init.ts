202984d25acbfd96b52ad275e5d0fc695deab619
domutala <ibntalla@gmail.com>
2022-12-15 18:04:59 +0000

    import css in init.ts

--- init.ts
+++ init.ts
@@ -2,6 +2,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { Mood } from "./index";
+import "@mood/styles/index.scss";
 
 export default function () {
   const app = createApp(App);
