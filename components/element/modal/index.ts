25619608bc5fb19b3a4869417b70d42c6d6fb30e
domutala <ibntalla@gmail.com>
2023-01-19 01:57:33 +0000

    modal

--- components/element/modal/index.ts
+++ components/element/modal/index.ts
@@ -69,8 +69,7 @@ export class MModal {
       this.back.style.top = "0";
       this.back.style.left = "0";
       this.back.style.zIndex = `${this.zIndex - 0}`;
-      this.back.style.backgroundColor =
-        this.type === "modal" ? "#0000000b" : "#ffffff00";
+      this.back.style.backgroundColor = "#00000029";
       this.back.style.width = "100%";
       this.back.style.height = "100%";
 
--- components/element/modal/type.d.ts
+++ components/element/modal/type.d.ts
@@ -2,7 +2,6 @@ import MModal from "./MModal.vue";
 import MModalBottom from "./MModalBottom.vue";
 import MModalPopup from "./MModalPopup.vue";
 import MModalOptions from "./MModalOptions.vue";
-import MModalOptions00 from "./MModalOptions00.vue";
 
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
@@ -10,6 +9,5 @@ declare module "@vue/runtime-core" {
     MModalBottom: typeof MModalBottom;
     MModalPopup: typeof MModalPopup;
     MModalOptions: typeof MModalOptions;
-    MModalOptions00: typeof MModalOptions00;
   }
 }
